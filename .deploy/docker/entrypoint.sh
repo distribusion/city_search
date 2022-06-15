aee6b256adb1c956e347c159f8347d41de960279
Artyom Fomenko <artyom.fomenko@distribusion.com>
2022-06-15 18:57:07 +0200

    Minor fixes

--- .deploy/docker/entrypoint.sh
+++ .deploy/docker/entrypoint.sh
@@ -14,8 +14,5 @@ city-search --verbose db-init
 echo "Upgrade database"
 city-search --verbose db-upgrade
 
-echo "Service users"
-city-search user-list
-
 # Start single gunicorn worker
 gunicorn -w 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout 120 city_search.main:app
